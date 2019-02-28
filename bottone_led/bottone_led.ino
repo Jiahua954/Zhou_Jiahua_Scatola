8cb3fa369f0a7db0e4ff7fe1ab748b28f4a31b1e
JiahuaZhou <jiahua.zhou@barsanti.edu.it>
2019-02-28 20:04:02 +0100

    Bottone+Led

---
+++ bottone_led/bottone_led.ino
@@ -0,0 +1,20 @@
+#define LED 13
+#define BUTTON 7
+int val = 0;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, OUTPUT);
+}
+
+void loop() {
+  val = digitalRead(BUTTON);
+  if(val == HIGH)
+  {
+    digitalWrite(LED, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED, LOW);
+  }
+}
